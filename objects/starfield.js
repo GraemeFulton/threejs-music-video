c92976154ef2b46be372ae8786aa11a99b9ac027
Graeme Fulton <graemefulton89@gmail.com>
2016-01-02 08:41:59 +0000

    video overlay

--- objects/starfield.js
+++ objects/starfield.js
@@ -6,79 +6,61 @@
 	  this.material = new THREE.MeshBasicMaterial( {color: 0xffffff} );
 	  this.starShape = new THREE.Mesh(this.geometry, this.material);
   }
-  
+
   /**
-   * Starfield 
+   * Starfield
    */
-  
+
   function StarField(){
     this.stars = [];
   }
-  
+
   /**
    * Starfield functions
    */
   StarField.prototype = {
-    
+
     constructor:StarField,
     addStars:function(scene){
-      // The loop will move from z position of -1000 to z position 1000, adding a random particle at each position. 
-      for ( var z= -1000; z < 1000; z+=20 ) {
-    
-          // Make a sphere (exactly the same as before). 
+      // The loop will move from z position of -1000 to z position 1000, adding a random particle at each position.
+      for ( var z= -50; z < 0; z+=5 ) {
+
+          // Make a sphere (exactly the same as before).
           var star = new Star();
           var sphere = star.starShape;
-    
+
           // This time we give the sphere random x and y positions between -500 and 500
-          sphere.position.x = this.randomBetween(-800, 800);
-          sphere.position.y = this.randomBetween(50, 300);
-    
+          sphere.position.x = this.randomBetween(-200,200);
+          sphere.position.y = this.randomBetween(-10, 50);
+
           // Then set the z position to where it is in the loop (distance of camera)
           sphere.position.z = z;
-    
+
           // scale it up a bit
-          sphere.scale.x = sphere.scale.y = 1.5;
-    
+
           //add the sphere to the scene
           scene.add( sphere );
-    
-          //finally push it to the stars array 
-          this.stars.push(sphere); 
+
+          //finally push it to the stars array
+          this.stars.push(sphere);
         }
     },
     animateStars:function(camera){
       // loop through each star
       for(var i=0; i<this.stars.length; i++) {
-        
-        var star = this.stars[i]; 
-          
-        // move it forward by a 50th of its array position each time 
-        star.position.z +=  i/50;   
-        
+
+        var star = this.stars[i];
+
+        // move it forward by a 50th of its array position each time
+        star.position.z +=  i;
+
        //if the camera has moved past the entire square, move the square
-        if((star.position.z - 800)>camera.position.z){
-            
+        if((star.position.z - 100)>camera.position.z){
+
             star.position.z-=(800*2);
           }
-         //if the camera has moved past the entire square in the opposite direction, move the square the opposite way
-          else if((star.position.z + 800)<camera.position.z){
-            
-            star.position.z+=(800*2);
-          } 
-       
-       
-       //x positions
-       if((star.position.x - 800)>camera.position.x){
-            
-            star.position.x-=(800*2);
-          }
-           //if the camera has moved past the entire square in the opposite direction, move the square the opposite way
-          else if((star.position.x + 800)<camera.position.x){
-            
-            star.position.x+=(800*2);
-          }
-        
-        
+
+
       }
     },
     /**
@@ -93,5 +75,5 @@
           return min + Math.random() * max;
       }
     }
-    
+
   };
